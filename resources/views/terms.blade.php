e078f9e368a376131874344929aee148b781ed88
Filip <filipivanovic59@gmail.com>
2024-08-31 12:48:11 +0200

    base config

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.layout')
+
+
+@section('content')
+<h1>Terms</h1>
+<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nihil porro quae pariatur enim omnis incidunt animi.
+     Earum fugit dicta cupiditate temporibus quibusdam minus saepe recusandae officiis? Modi, itaque nemo!</div>
+@endsection
