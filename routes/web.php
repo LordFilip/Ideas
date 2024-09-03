580b250ff800d353e81ade31a1a1e81e81bf252a
Filip <filipivanovic59@gmail.com>
2024-09-03 10:47:32 +0200

    authentication

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\AuthController;
+
 
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
@@ -20,6 +22,19 @@ Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.store');
 Route::delete('/ideas/{id}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
 
 Route::post('/ideas/{idea}/comments', [CommentController::class, 'store'])->name('ideas.comments.store');
+
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+
+Route::post('/register', [AuthController::class, 'store']);
+
+Route::get('/login',[AuthController::class,'login'])->name('login');
+
+Route::post('/login',[AuthController::class,'authenticate']);
+
+Route::post('/logout',[AuthController::class,'logout'])->name('logout');
+
+
+
 
 
 
